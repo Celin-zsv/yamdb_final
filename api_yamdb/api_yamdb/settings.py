f9e574b4ea14ccfed95ab1654ea75033476e2143
Сергей Зеленковский <svzelenkoskii@gmail.com>
2023-01-16 12:32:04 +0300

    executing remote ssh commands to deploy v18_6

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -120,6 +120,7 @@ FROM_EMAIL = 'admin@yamdb.com'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
